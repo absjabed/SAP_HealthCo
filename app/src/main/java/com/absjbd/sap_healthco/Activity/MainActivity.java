d20bbb233d48410b8bc7f30e1f871afd96298a41
aney biswas <aney.biswas616@gmail.com>
2018-02-21 15:59:30 +0600

    mainactivity changes by aney...

--- app/src/main/java/com/absjbd/sap_healthco/Activity/MainActivity.java
+++ app/src/main/java/com/absjbd/sap_healthco/Activity/MainActivity.java
@@ -13,7 +13,8 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);requestWindowFeature(Window.FEATURE_NO_TITLE);
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 //        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
